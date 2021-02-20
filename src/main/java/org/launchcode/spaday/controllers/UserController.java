293b6ffdce42b1da47f7703dadd1ba51e62ac5a0
Domonique Taylor <taylord651@gmail.com>
2021-02-20 11:56:52 -0600

    Completed part 4 of Spa User Signup Studio

--- src/main/java/org/launchcode/spaday/controllers/UserController.java
+++ src/main/java/org/launchcode/spaday/controllers/UserController.java
@@ -1,7 +1,11 @@
 package org.launchcode.spaday.controllers;
 
+import org.launchcode.spaday.models.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -11,6 +15,18 @@ public class UserController {
     @GetMapping
     public String displayAddUserForm() {
         return "user/add";
+    }
+
+    @PostMapping
+    public String processAddUserForm(Model model, @ModelAttribute User user, String verifyPassword) {
+        //User.add(user);
+        if (!user.getPassword().equals(verifyPassword)) {
+            model.addAttribute("error","Error, passwords do not match");
+            return "user/add";
+        } else {
+            model.addAttribute("user", user);
+            return "user/index";
+        }
     }
 
 }
