7b2ebefde825ddd003be4618e8b8f43801736ed7
Domonique Taylor <taylord651@gmail.com>
2021-02-27 11:18:22 -0600

    Completed Spa User Validation Studio (13)

--- src/main/java/org/launchcode/spaday/models/User.java
+++ src/main/java/org/launchcode/spaday/models/User.java
@@ -1,8 +1,20 @@
 package org.launchcode.spaday.models;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 public class User {
+
+    @NotBlank(message = "Username required")
+    @Size(min = 5, max = 15, message = "Username must be between 5 and 15 characters")
     private String username;
+
+    @Email(message = "Must be a valid email address")
     private String email;
+
+    @NotBlank(message = "Password required")
+    @Size(min = 6, message = "Password must be at least 6 characters")
     private String password;
 
     public User() {
